f61eb1812b183bdda1d1f7ee0f330d9fd9c20c00
Nazgul <bizdanistar@gmail.com>
2024-09-29 15:10:02 -0500

    deployed

--- Desktop/shop-project-react/vite.config.js
+++ Desktop/shop-project-react/vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "https://bizdanistar.github.io/ecommerce-project.git"
 })
